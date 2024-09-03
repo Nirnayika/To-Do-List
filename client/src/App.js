7405ed0fee7c106040465b1ab6d885c8642955b5
Nirnayika Rai <128626977+Nirnayika@users.noreply.github.com>
2024-09-03 20:38:28 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+
+//components
+import Header from './components/Header';
+import TodoForm from './components/TodoForm';
+import Todos from './components/Todos';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+      <Header />
+      <TodoForm />
+      <Todos />
+      </div>
+    </div>
+  );
+}
+
+export default App;
